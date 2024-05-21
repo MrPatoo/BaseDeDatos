a0c37efd3f86a591c62961c003b63598b906b40d
Rebekin <107485480+Rebekin@users.noreply.github.com>
2024-05-21 15:31:24 -0600

    boton de eliminar funcional

--- app/src/main/java/RecycleViewHelpers/Adaptador.kt
+++ app/src/main/java/RecycleViewHelpers/Adaptador.kt
@@ -1,5 +1,7 @@
 package RecyclerViewHelper
 
+import Modelo.Conexion
+import android.app.AlertDialog
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
@@ -17,6 +19,39 @@ class Adaptador(private var Datos: List<dataClassMascotas>) : RecyclerView.Adapt
         Datos = nuevaLista
         notifyDataSetChanged()//Esto notifica al recyclerview que hay datos nuevos
     }
+
+    ///////////////////////////TODO: ELIMINAR DATOS///////////////////////////////
+
+    fun eliminarDatos(nombreMascotas: String, position: Int){
+
+        //actualizar lista de datos y notifico al adaptador
+        val listaDatos = Datos.toMutableList()
+        listaDatos.removeAt(position)
+
+
+        GlobalScope.launch (Dispatchers.IO){
+            //1- crear un objeto de la clase conexion
+            val objConexion = Conexion().cadenaConexion()
+
+            //2- crear una variable que contenga un PrepareStatement
+            val deleteMascota = objConexion?.prepareStatement("delete from tbMascotas where nombreMascota = ?")!!
+            deleteMascota.setString(1, nombreMascotas)
+            deleteMascota.executeUpdate()
+
+            val commit = objConexion.prepareStatement("commit")!!
+            commit.executeUpdate()
+
+        }
+        Datos = listaDatos.toList()
+        //notifico al adaptador sobre los cambios
+        notifyItemRemoved(position)
+        notifyDataSetChanged()
+
+
+
+    }
+
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val vista =
             LayoutInflater.from(parent.context).inflate(R.layout.activity_item_card, parent, false)
@@ -26,8 +61,37 @@ class Adaptador(private var Datos: List<dataClassMascotas>) : RecyclerView.Adapt
     override fun getItemCount() = Datos.size
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val producto = Datos[position]
-        holder.textView.text = producto.nombreMascota
+        val mascota = Datos[position]
+        holder.textView.text = mascota.nombreMascota
+
+        //todo click en el icono de eliminar
+        holder.btnEliminar.setOnClickListener{
+
+            //creamos Alert
+            val context = holder.itemView.context
+
+            val builder = AlertDialog.Builder(context)
+            builder.setTitle("Eliminar")
+            builder.setMessage("¿Desea eliminar la mascota?")
+
+            //botones
+            builder.setPositiveButton("si"){dialog, switch -> eliminarDatos(mascota.nombreMascota, position)
+
+            }
+            builder.setNegativeButton("no"){dialog, which -> dialog.dismiss()
+
+            }
+
+            val dialog = builder.create()
+            dialog.show()
+
+
+
+
+
+        }
+
+
     }
 
 
--- app/src/main/java/RecycleViewHelpers/ViewHolder.kt
+++ app/src/main/java/RecycleViewHelpers/ViewHolder.kt
@@ -6,7 +6,8 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import rodrigo.cordova.crudrodrigoc2b.R
 
-class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+class
+ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
     val textView: TextView = view.findViewById(R.id.txtMascotaCard)
     val btnEditar: ImageView = view.findViewById(R.id.imgEditar)
     val btnEliminar: ImageView = view.findViewById(R.id.imgEliminar)
